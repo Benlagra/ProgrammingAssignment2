507fe314752bcf1903d244308ac0e9e14cd1032a
Adel Benlagra <benlagra@itp.phys.ethz.ch>
2014-04-18 18:26:00 +0200

    Corrected the layout of the source file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,22 @@
 ## 
 ##  The code defines two functions used to cache a matrix and its inverse. 
-##  The first function creates an extended 'matrix' object which retains the information about 
-##  the matrix and its inverse. The second fonctions, whose argument is the extended object,
-##   is a cached version of the function Solve. It returns the cached or computed value for the inverse
-##   matrix.
+##  The first function creates an extended 'matrix' object which retains the 
+##  information about the matrix and its inverse. The second fonctions, whose 
+##  argument is the extended object, is a cached version of the function Solve. 
+##  It returns the cached or computed value for the inverse matrix.
 ##  
 
 
 
 
-## This function creates an extended 'Matrix' object in the form of a list of functions which
-## keep the information about the matrix argument x and its inverse. These functions are:
+## This function creates an extended 'Matrix' object in the form of a list of 
+##functions which keep the information about the matrix argument x and its inverse. 
+## These functions are:
 ##
 ## * set(y) resets the matrix x to be inverted to the value y and its inverse to NULL.
 ## * get() returns the cached matrix x.
-## * getinverse() returns the cached inverse matrix or NULL if the former was not already computed.
+## * getinverse() returns the cached inverse matrix or NULL if the former was not 
+##     already computed.
 ## * setinverse(invserse) sets the new value of the inverse matrix.
 
 makeCacheMatrix <- function(xm = matrix()) {
@@ -24,8 +26,9 @@ makeCacheMatrix <- function(xm = matrix()) {
        
        set <- function(ym) {
                      xm <<- ym
-                     ixm <<- NULL        ## Inverse value stored in Extended Matrix object reset to NULL
-                                         ## whenever the matrix xm is reset.
+                     ixm <<- NULL        ## Inverse value stored in Extended  
+                                         ## Matrix object reset to NULL whenever 
+                                         ## the matrix xm is reset.
        }
        get <- function() xm
        setinverse <- function(inverse) ixm <<- inverse
@@ -37,18 +40,20 @@ makeCacheMatrix <- function(xm = matrix()) {
 }
 
 
-## This functions is an extension to the base function Solve. It calculates the inverse of a matrix
-## only if this has not already beeen done earlier. It acts on the extended object defined in the 
-## function above and sets the value of the inverse matrix in that object if this was not done 
+## This functions is an extension to the base function Solve. It calculates 
+## the inverse of a matrix only if this has not already beeen done earlier. 
+## It acts on the extended object defined in the function above and sets the 
+## value of the inverse matrix in that object if this was not done 
 ## before.
 
 cacheSolve <- function(x, ...) {
         
-       # Calls the value of the inverse matrix cached in the extended Matrix object x.
+       # Calls the value of the inverse matrix cached in the extended Matrix 
+       # object x.
        ix <- x$getinverse()
        
-       # If this is different from NULL, the inverse matrix has already been calculated 
-       # and we can use its cached value.
+       # If this is different from NULL, the inverse matrix has already been 
+       # calculated and we can use its cached value.
        if(!is.null(ix)) {
               message("Inverse already calculated ! \n Getting cached data")
               return(ix)
